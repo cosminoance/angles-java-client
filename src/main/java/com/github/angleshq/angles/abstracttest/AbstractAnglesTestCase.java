4038bbf5d3a84840f821eabf9e2262746c92feb8
Srana Boonlertuthai <srana.boonlertuthai@roxorgaming.com>
2021-03-29 18:30:59 +0100

    extracted some functionality out of the AbstractTestCase to AnglesUtils

--- src/main/java/com/github/angleshq/angles/abstracttest/AbstractAnglesTestCase.java
+++ src/main/java/com/github/angleshq/angles/abstracttest/AbstractAnglesTestCase.java
@@ -3,34 +3,22 @@ package com.github.angleshq.angles.abstracttest;
 import com.github.angleshq.angles.AnglesReporter;
 import com.github.angleshq.angles.exceptions.AnglesPropertyNotGivenException;
 
-import static com.github.angleshq.angles.util.AnglesUtils.getAnglesPropertyFromSystem;
+import static com.github.angleshq.angles.util.AnglesUtils.getComponent;
+import static com.github.angleshq.angles.util.AnglesUtils.getEnvironment;
+import static com.github.angleshq.angles.util.AnglesUtils.getRunName;
+import static com.github.angleshq.angles.util.AnglesUtils.getTeam;
+import static com.github.angleshq.angles.util.AnglesUtils.initialiseAnglesTestParameters;
 
 public abstract class AbstractAnglesTestCase {
 
     protected AnglesReporter anglesReporter;
-    protected String runName;
-    protected String team;
-    protected String component;
-    protected String environment;
 
     protected AbstractAnglesTestCase() {
         try {
-            initialiseAnglesTestParameters();
-            anglesReporter.startBuild(runName, environment, team, component);
+            anglesReporter = initialiseAnglesTestParameters();
+            anglesReporter.startBuild(getRunName(), getEnvironment(), getTeam(), getComponent());
         } catch (AnglesPropertyNotGivenException e) {
             System.out.println("Please ensure you provide all required system properties for Angles Dashboard.");
         }
-    }
-
-    protected void initialiseAnglesTestParameters() throws AnglesPropertyNotGivenException {
-        anglesReporter = AnglesReporter.getInstance(getAnglesPropertyFromSystem("angles.url") + "/rest/api/v1.0/");
-        runName = getAnglesPropertyFromSystem("angles.runName");
-        team = getAnglesPropertyFromSystem("angles.team");
-        component = getAnglesPropertyFromSystem("angles.component");
-        environment = getAnglesPropertyFromSystem("angles.environment");
-    }
-
-    protected String getUniqueTestName(String suiteName, String methodName) {
-        return suiteName + "_" + methodName;
     }
 }
--- src/main/java/com/github/angleshq/angles/util/AnglesUtils.java
+++ src/main/java/com/github/angleshq/angles/util/AnglesUtils.java
@@ -1,10 +1,67 @@
 package com.github.angleshq.angles.util;
 
+import com.github.angleshq.angles.AnglesReporter;
 import com.github.angleshq.angles.exceptions.AnglesPropertyNotGivenException;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class AnglesUtils {
+
+    private static AnglesReporter anglesReporter;
+    private static String runName;
+    private static String team;
+    private static String component;
+    private static String environment;
+
+    public static AnglesReporter getAnglesReporter() {
+        return anglesReporter;
+    }
+
+    public static void setAnglesReporter(AnglesReporter anglesReporter) {
+        AnglesUtils.anglesReporter = anglesReporter;
+    }
+
+    public static String getRunName() {
+        return runName;
+    }
+
+    public static void setRunName(String runName) {
+        AnglesUtils.runName = runName;
+    }
+
+    public static String getTeam() {
+        return team;
+    }
+
+    public static void setTeam(String team) {
+        AnglesUtils.team = team;
+    }
+
+    public static String getComponent() {
+        return component;
+    }
+
+    public static void setComponent(String component) {
+        AnglesUtils.component = component;
+    }
+
+    public static String getEnvironment() {
+        return environment;
+    }
+
+    public static void setEnvironment(String environment) {
+        AnglesUtils.environment = environment;
+    }
+
+    public static AnglesReporter initialiseAnglesTestParameters() throws AnglesPropertyNotGivenException {
+        anglesReporter = AnglesReporter.getInstance(getAnglesPropertyFromSystem("angles.url") + "/rest/api/v1.0/");
+        runName = getAnglesPropertyFromSystem("angles.runName");
+        team = getAnglesPropertyFromSystem("angles.team");
+        component = getAnglesPropertyFromSystem("angles.component");
+        environment = getAnglesPropertyFromSystem("angles.environment");
+        return anglesReporter;
+    }
+
     public static String getAnglesPropertyFromSystem(String property) throws AnglesPropertyNotGivenException {
         String propertyValue = System.getProperty(property);
         if(isBlank(propertyValue)) {
