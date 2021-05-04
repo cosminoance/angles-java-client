1e673b92c701efed1407553088f03693f8ca3e38
Cosmin Oance <Cosmin.Oance@roxorgaming.com>
2021-05-04 13:32:49 +0100

    adding step logging (no argument list)

--- src/main/java/com/github/angleshq/angles/basetest/cucumber/AnglesCucumber2Adapter.java
+++ src/main/java/com/github/angleshq/angles/basetest/cucumber/AnglesCucumber2Adapter.java
@@ -4,12 +4,29 @@ import cucumber.api.Result;
 import cucumber.api.TestStep;
 import cucumber.api.event.EventHandler;
 import cucumber.api.event.TestCaseFinished;
+import cucumber.api.event.TestStepFinished;
+import cucumber.api.event.TestStepStarted;
 import cucumber.api.formatter.Formatter;
+import cucumber.runner.PickleTestStep;
 import gherkin.pickles.PickleStep;
 
 
 public class AnglesCucumber2Adapter extends AnglesCucumberHooks implements Formatter {
 
+    protected EventHandler<TestStepStarted> handleTestStepFinished = new EventHandler<TestStepStarted>() {
+        @Override
+        public void receive(TestStepStarted event) {
+            TestStep testStep = event.testStep;
+            if (testStep.getClass() == PickleTestStep.class) {
+                String text = event.testStep.getStepText();
+                String argument = testStep.getStepArgument().toString();
+                if(testStep.getStepArgument().size()>0) {
+                    anglesReporter.info("Step: " + text + argument);
+                }
+
+            }
+        }
+    };
 
     protected EventHandler<TestCaseFinished> handleTestCaseFinished =
             new EventHandler<TestCaseFinished>() {
@@ -25,20 +42,25 @@ public class AnglesCucumber2Adapter extends AnglesCucumberHooks implements Forma
 
                     String id = "" + testCase.getUri() + testCase.getLine();
                     System.out.println("Testcase " + id + " - " + result.getStatus());
-                    if(result.getStatus().equals(Result.Type.PASSED)){
-                        anglesReporter.pass(scenarioName +" passeed!","","","");
+                    if (result.getStatus().equals(Result.Type.PASSED)) {
+                        anglesReporter.pass(scenarioName + " passed!", "", "", "");
                     }
-                    if (testStep.isHook()) {
-                        anglesReporter.info("Finished  scenario" + scenarioName);
-                        anglesReporter.saveTest();
+                    if (result.getStatus().equals(Result.Type.FAILED)) {
+                        anglesReporter.fail(scenarioName + " passed!", "", "", "");
                     }
-                }
+                    if (result.getStatus().equals(Result.Type.SKIPPED)) {
+                        anglesReporter.fail(scenarioName + " passed!", "", "", "");
+                    }
 
+                    anglesReporter.saveTest();
+
+                }
             };
 
     @Override
     public void setEventPublisher(cucumber.api.event.EventPublisher publisher) {
         publisher.registerHandlerFor(cucumber.api.event.TestCaseFinished.class, handleTestCaseFinished);
+        publisher.registerHandlerFor(cucumber.api.event.TestStepStarted.class, handleTestStepFinished);
     }
 }
 
